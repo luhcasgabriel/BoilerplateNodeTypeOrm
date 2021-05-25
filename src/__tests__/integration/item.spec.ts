37abace74b291fab40ee2c6bc64a579f5bb7f606
Lais Frigério <laaisfrigerio@gmail.com>
2021-05-25 01:55:03 -0300

    Items tests

--- src/__tests__/integration/item.spec.ts
+++ src/__tests__/integration/item.spec.ts
@@ -1,11 +1,51 @@
 import request from 'supertest'
+import { create, close, clear } from '../../database'
 import AppController from '../../app'
 
 const app = new AppController().app
 
+beforeAll(async ()=>{
+    await create()
+})
+
+afterAll(async ()=>{
+    await close()
+})
+
+beforeEach(async () => {
+    clear()
+})
+
 describe('Items', () => {
-    it('should search for valid items in the database', async () => {
-        const response = await request(app).get('/items')
-        expect(response.status).toEqual(200)
+    it('should return items with 200 status code', async () => {
+        const responseGet = await request(app).get('/items')
+        expect(responseGet.body.length).toEqual(0)
+        expect(responseGet.status).toEqual(200)
+    })
+
+    it('should save one item', async () => {
+        const item = { name: 'Alface', price: 0.40 }
+
+        const responsePost = await request(app).post('/items').send(item)
+
+        expect(responsePost.status).toEqual(200)
+        expect(responsePost.body.name).toEqual(item.name)
+        expect(responsePost.body.price).toEqual(item.price)
+        expect(responsePost.body.id).not.toBeNull()
+        expect(responsePost.body.createdAt).not.toBeNull()
+        expect(responsePost.body.updatedAt).not.toBeNull()
+
+        const responseGet = await request(app).get('/items')
+        expect(responseGet.body.length).toEqual(1)
+        expect(responseGet.status).toEqual(200)
+    })
+
+    it('should save two items', async () => {
+        await request(app).post('/items').send({ name: 'Ovo', price: 0.80 })
+        await request(app).post('/items').send({ name: 'Queijo', price: 1.00 })
+
+        const responseGet = await request(app).get('/items')
+        expect(responseGet.body.length).toEqual(2)
+        expect(responseGet.status).toEqual(200)
     })
 })
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,27 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnection } from 'typeorm'
 
-createConnection();
+const create = async () => {
+  return await createConnection()
+}
+
+const close = async () => {
+  await getConnection().close()
+}
+
+const clear = async () => {
+  const connection = getConnection()
+  const entities = connection.entityMetadatas
+
+  entities.forEach(async (entity) => {
+    const repository = connection.getRepository(entity.name)
+    await repository.query(`DELETE FROM ${entity.tableName}`)
+  })
+}
+
+create()
+
+export {
+  create,
+  close,
+  clear
+}
