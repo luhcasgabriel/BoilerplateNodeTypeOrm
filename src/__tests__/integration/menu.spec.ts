a505373270fc5103a2a6875b45859d049f786827
Lais Frigério <laaisfrigerio@gmail.com>
2021-05-25 02:18:16 -0300

    Tests menu

--- src/__tests__/integration/menu.spec.ts
+++ src/__tests__/integration/menu.spec.ts
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import { create, close, clear } from '../../database'
 import { alfaceItem, ovoItem, queijoItem } from './payload/items'
+import { xSaladaMenu } from './payload/menus'
 import AppController from '../../app'
 
 const app = new AppController().app
@@ -17,33 +18,50 @@ beforeEach(async () => {
     clear()
 })
 
+const createItems = async () => {
+  const items = [ alfaceItem, ovoItem, queijoItem ]
+
+  for (const item of items) {
+    await request(app).post('/items').send(item)
+  }
+}
+
 describe('Menu', () => {
-    it('should return menus with 200 status code', async () => {
-        const responseGet = await request(app).get('/menu')
-        expect(responseGet.body.length).toEqual(0)
-        expect(responseGet.status).toEqual(200)
-    })
+  it('should return menus with 200 status code', async () => {
+    const responseGet = await request(app).get('/menu')
+    expect(responseGet.body.length).toEqual(0)
+    expect(responseGet.status).toEqual(200)
+  })
 
-    it('should save one item', async () => {
-      const items = [ alfaceItem, ovoItem, queijoItem ]
+  it('should save one menu', async () => {
+    await createItems()
 
-      for (const item of items) {
-        await request(app).post('/items').send(item)
-      }
+    let responseGet = await request(app).get('/items')
+    const { body, status } = responseGet
+    expect(body.length).toEqual(3)
+    expect(status).toEqual(200)
 
-      const responseGet = await request(app).get('/items')
-      const { body, status } = responseGet
-      expect(body.length).toEqual(3)
-      expect(status).toEqual(200)
+    const menu = { ...xSaladaMenu, items: body }
+    const responsePost = await request(app).post('/menu').send(menu)
 
-      const menu = { name: 'X-salada', items: body }
-      const responsePost = await request(app).post('/menu').send(menu)
+    expect(responsePost.status).toEqual(200)
+    expect(responsePost.body.name).toEqual(menu.name)
+    expect(responsePost.body.items.length).toEqual(3)
+    expect(responsePost.body.id).not.toBeNull()
+    expect(responsePost.body.createdAt).not.toBeNull()
+    expect(responsePost.body.updatedAt).not.toBeNull()
+  })
 
-      expect(responsePost.status).toEqual(200)
-      expect(responsePost.body.name).toEqual(menu.name)
-      expect(responsePost.body.items.length).toEqual(3)
-      expect(responsePost.body.id).not.toBeNull()
-      expect(responsePost.body.createdAt).not.toBeNull()
-      expect(responsePost.body.updatedAt).not.toBeNull()
+  it('should not found menu', async () => {
+    const responseGet = await request(app).get('/menu/1')
+    expect(responseGet.status).toEqual(204)
+  })
+
+  it('should save menus with 0 items and find it', async () => {
+    const responsePost = await request(app).post('/menu').send(xSaladaMenu)
+    const { body } = responsePost
+
+    const responseGet = await request(app).get(`/menu/${body.id}`)
+    expect(responseGet.status).toEqual(200)
   })
 })
