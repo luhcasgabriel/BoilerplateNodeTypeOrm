d6891b8db2c70e89055cd3ccb0d0178e30b258ed
Lais Frigério <laaisfrigerio@gmail.com>
2021-05-25 00:56:43 -0300

    tests

--- src/__tests__/integration/item.spec.ts
+++ src/__tests__/integration/item.spec.ts
@@ -1,29 +1,11 @@
-import { getCustomRepository } from "typeorm";
-import { Item } from "../../entities/Item";
-import { ItemsRepository } from "../../repositories/ItemsRepository";
-import request from "supertest";
-import { AppController } from "../../app"
+import request from 'supertest'
+import AppController from '../../app'
 
-const app = new AppController().app;
+const app = new AppController().app
 
-describe("Items", () => {
-
-    
+describe('Items', () => {
     it('should search for valid items in the database', async () => {
-
-        // const itemsRepository = getCustomRepository(ItemsRepository);
-        
-        /*const item = await  itemsRepository.create({ 
-            name: , 
-            price
-        })*/
-        
-        const response = await request(app).get("/items")
-            
-
-    
-        expect(response.status).toEqual(200);
-
-
-    });
+        const response = await request(app).get('/items')
+        expect(response.status).toEqual(200)
+    })
 })
